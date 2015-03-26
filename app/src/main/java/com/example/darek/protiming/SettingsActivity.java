74739131c8c89e806dfdd10af665195217c21ae1
dyziaster <dyzio777@tlen.pl>
2015-03-26 20:10:14 +0100

    implemented listview

--- app/src/main/java/com/example/darek/protiming/SettingsActivity.java
+++ app/src/main/java/com/example/darek/protiming/SettingsActivity.java
@@ -9,12 +9,17 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -24,25 +29,58 @@ public class SettingsActivity extends Activity{
     private Button discoverButton;
     private BluetoothManager manager;
     private Set<BluetoothDevice> devices;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
+    private String[] tmp = new String[]{"awdawda","awdwada"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) { // must enable BT first
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
         manager = BluetoothManager.getInstance();
+        /*
         if(!manager.bluetoothEnabled()){
             Log.i("SettingsActivity","bluetooth turned off");
             return;
         }
-
+        */
         discoverButton = (Button)findViewById(R.id.discoverButton);
         discoverButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-                devices = manager.getBondedDevices();
+                //devices = manager.getBondedDevices();
+                populateList(); // need to implement return from BT devices>tostring>toarrayadapter.
                 Toast.makeText(getApplicationContext(), "discovering", Toast.LENGTH_LONG).show();
             }
         });
+
+        listView = (ListView) findViewById(R.id.listview);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        //listView.setOnItemClickListener();
+    }
+
+    private void populateList(){
+
+
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,tmp);
+        listView.setAdapter(arrayAdapter);
+
     }
 
     @Override
